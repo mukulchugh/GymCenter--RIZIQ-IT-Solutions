1dbd731d406ff98d82c8952e57e3cbd6d79966e9
Ayon Jodder <ayonjodder177@gmail.com>
2022-08-23 19:26:50 +0600

    Header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div>
-     
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   );
 }
