2eca8fa369a1f88559d4113063274a5879a023eb
Ayon Jodder <ayonjodder177@gmail.com>
2022-08-24 19:04:15 +0600

    Nav fix

--- src/Components/Home/Header/Navbar.js
+++ src/Components/Home/Header/Navbar.js
@@ -10,22 +10,22 @@ const Navbar = () => {
             </div>
             <div class="navbar-end hidden lg:flex">
                 <ul class="menu menu-horizontal p-0">
-                    <li><a>Home</a></li>
-                    <li><a>About</a></li>
+                    <li><a className="uppercase font-semibold" href="#">Home</a></li>
+                    <li><a className="uppercase font-semibold" href="#">About</a></li>
                     <li tabindex="0">
-                        <a>
+                        <a className='uppercase font-semibold'>
                             CrossFit
                             <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
                         </a>
                         <ul class="p-2">
-                            <li><a>Fitness</a></li>
-                            <li><a>Trainers</a></li>
-                            <li><a>Testimonials</a></li>
+                            <li><a className="uppercase font-semibold" href="#">Fitness</a></li>
+                            <li><a className="uppercase font-semibold" href="#">Trainers</a></li>
+                            <li><a className="uppercase font-semibold" href="#">Testimonials</a></li>
                         </ul>
                     </li>
-                    <li><a>Blog</a></li>
-                    <li><a>Shop</a></li>
-                    <li><a>Contact</a></li>
+                    <li><a className="uppercase font-semibold" href="#">Blog</a></li>
+                    <li><a className="uppercase font-semibold" href="#">Shop</a></li>
+                    <li><a className="uppercase font-semibold" href="#">Contact</a></li>
                 </ul>
             </div>
             <div className='navbar-end lg:hidden '>
@@ -34,11 +34,11 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow  rounded-box w-52 bg-transparent">
-                        <li><a>Home</a></li>
-                        <li><a>About</a></li>
-                        <li><a>Blog</a></li>
-                        <li><a>Shop</a></li>
-                        <li><a>Contact</a></li>
+                        <li><a className="uppercase font-semibold" href="#">Home</a></li>
+                        <li><a className="uppercase font-semibold" href="#">About</a></li>
+                        <li><a className="uppercase font-semibold" href="#">Blog</a></li>
+                        <li><a className="uppercase font-semibold" href="#">Shop</a></li>
+                        <li><a className="uppercase font-semibold" href="#">Contact</a></li>
                     </ul>
                 </div>
             </div>
