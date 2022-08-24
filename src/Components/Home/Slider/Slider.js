f9c313e298d0c37c2bf5f0cc5578f629b6301801
Md Imran Hossen <freelancerimran81@gmail.com>
2022-08-24 20:59:22 +0600

    slider updated

--- src/Components/Home/Slider/Slider.js
+++ src/Components/Home/Slider/Slider.js
@@ -33,7 +33,7 @@ export default function App() {
                     <div className="image-slide-bgOne relative md:px-10 md:py-24 px-5 py-20 md:flex items-center justify-between">
                         <div className="md:w-[70%] flex">
                             <img className="quote_image relative top-[-35px]  quote_image_one inline-block" src={quote} alt="" />
-                            <span className="text-white italic mx-auto">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+                            <span className="text-white italic mx-auto">This gym is amazing. Amazing facility, top equipment and great environment. Good service with friendly staff and super clean and comfortable gym for everyone from beginners to professional</span>
                             <img className="quote_image quote_image_two inline-block align-bottom -mb-5" src={quote} alt="" />
                         </div>
                         <div className="md:w-[25%] lg:mr-[-35px] mt-8 md:mt-0 flex flex-col items-center justify-center">
@@ -48,7 +48,7 @@ export default function App() {
                     <div className="image-slide-bgOne relative md:px-10 md:py-24 px-5 py-20 md:flex items-center justify-between">
                         <div className="md:w-[70%] flex">
                             <img className="quote_image relative top-[-35px]  quote_image_one inline-block" src={quote} alt="" />
-                            <span className="text-white italic mx-auto">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+                            <span className="text-white italic mx-auto">This gym is amazing. Amazing facility, top equipment and great environment. Good service with friendly staff and super clean and comfortable gym for everyone from beginners to professional.</span>
                             <img className="quote_image quote_image_two inline-block align-bottom -mb-5" src={quote} alt="" />
                         </div>
                         <div className="md:w-[25%] lg:mr-[-35px] mt-8 md:mt-0 flex flex-col items-center justify-center">
@@ -63,7 +63,7 @@ export default function App() {
                     <div className="image-slide-bgOne relative md:px-10 md:py-24 px-5 py-20 md:flex items-center justify-between">
                         <div className="md:w-[70%] flex">
                             <img className="quote_image relative top-[-35px]  quote_image_one inline-block" src={quote} alt="" />
-                            <span className="text-white italic mx-auto">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</span>
+                            <span className="text-white italic mx-auto">This gym is amazing. Amazing facility, top equipment and great environment. Good service with friendly staff and super clean and comfortable gym for everyone from beginners to professional</span>
                             <img className="quote_image quote_image_two inline-block align-bottom -mb-5" src={quote} alt="" />
                         </div>
                         <div className="md:w-[25%] lg:mr-[-35px] mt-8 md:mt-0 flex flex-col items-center justify-center">
