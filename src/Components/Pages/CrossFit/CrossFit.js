6a5eaa84d5deb962dd32b7763a8246f82923ede8
Md Imran Hossen <freelancerimran81@gmail.com>
2022-08-25 16:01:04 +0600

    button updated

--- src/Components/Pages/CrossFit/CrossFit.js
+++ src/Components/Pages/CrossFit/CrossFit.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import img1 from '../../../assets/Image/CrossFit/pic1.png'
 import img2 from '../../../assets/Image/CrossFit/pic2.png'
+import './CrossFit.css'
 
 const CrossFit = () => {
     return (
@@ -11,7 +12,7 @@ const CrossFit = () => {
                         <h1 className='text-3xl text-primary font-extrabold mb-5'>WHAT IS CROSSFIT?</h1>
                         <p className='font-bold lg:w-[90%] '>We believe fitness should be accessible to everyone, everywhere, regardless of income or access to a gym. With hundreds of professional workouts, healthy recipes and informative articles, as well as one of the most positive communities on the web, you’ll have everything you need to reach your personal fitness goals – for free!</p>
 
-                        <h1 className='uppercase text-primary font-bold mt-5 cursor-pointer'>see more</h1>
+                        <button className='custom-btn btn-15 mt-5 p-3'>see more</button>
                     </div>
                 </div>
                 <div>
@@ -27,7 +28,8 @@ const CrossFit = () => {
                         <h1 className='text-3xl text-primary font-extrabold mb-5'>All sessions are led by a fully qualified CrossFit coach.</h1>
                         <p className='font-bold lg:w-[90%] '>We believe fitness should be accessible to everyone, everywhere, regardless of income or access to a gym. With hundreds of professional workouts, healthy recipes and informative articles, as well as one of the most positive communities on the web, you’ll have everything you need to reach your personal fitness goals – for free!</p>
 
-                        <h1 className='uppercase text-primary font-bold mt-5 cursor-pointer'>see more</h1>
+                        {/* <h1 className='uppercase text-primary font-bold mt-5 cursor-pointer'>see more</h1> */}
+                        <button className='custom-btn btn-15 mt-5 p-3'>see more</button>
                     </div>
                 </div> 
 
