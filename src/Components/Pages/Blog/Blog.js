a5a9b9a460ce929396880b6164f0e2c5d8d46558
Md Imran Hossen <freelancerimran81@gmail.com>
2022-08-27 14:24:52 +0600

    short read section done

--- src/Components/Pages/Blog/Blog.js
+++ src/Components/Pages/Blog/Blog.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SharedNav from '../Shared/SharedNav';
+import ShortReads from './ShortReads/ShortReads';
 import Trending from './Trending/Trending';
 
 const Blog = () => {
@@ -8,6 +9,7 @@ const Blog = () => {
             <SharedNav />
             <div className='mid-container h-[100vh]'>
                 <Trending />
+                <ShortReads/>
             </div>
         </>
     );
