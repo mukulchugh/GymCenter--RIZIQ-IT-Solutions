f53d2c242b9aa44d4194dc5b0e938c440d4bbe8b
Md Imran Hossen <freelancerimran81@gmail.com>
2022-08-27 18:39:00 +0600

    login page update

--- src/Components/Pages/Shared/FigmaLogin.js
+++ src/Components/Pages/Shared/FigmaLogin.js
@@ -8,39 +8,39 @@ const FigmaLogin = () => {
     return (
         <>
             <SharedNav />
-            <div className='mid-container h-[100vh] flex items-center justify-evenly'>
-                <div className="login_image hidden md:w-1/2 md:block">
-                    <img src={LoginImage} alt="" />
+            <div className='flex  justify-between h-[92vh]'>
+                <div className="login_image md:w-[60%] hidden  md:block">
+                    <img className="w-full h-full" src={LoginImage} alt="" />
                 </div>
-                <div className="login_form w-full md:w-1/2">
+                <div className="login_form md:w-[40%] w-full px-5 mt-10 ">
 
                     <img className='mx-auto' src={Logo} alt="" />
-                    <div class="text text-center mt-2">Nice to see you again</div>
+                    <h1 class="text-xl font-bold text-center mt-2 mb-3">Nice to see you again!</h1>
 
 
-                    <form action="#">
-                        <div className="flex sm:w-2/3 w-full mx-auto flex-col">
+                    <form className="lg:w-[85%] md:w-full sm:w-2/3 w-full mx-auto" action="#">
+                        <div className="flex  w-full mx-auto flex-col">
                             <label className='text-[#747474] text-sm font-medium ml-4 mb-2' for="email">Login</label>
                             <input className='py-3 px-5 bg-[#F2F2F2] rounded-md' type="email" name="email" id="" placeholder='Email or phone number' />
                         </div>
-                        <div className="flex sm:w-2/3 w-full mx-auto flex-col mt-5">
+                        <div className="flex  w-full mx-auto flex-col mt-5">
                             <label className='text-[#747474] text-sm font-medium ml-4 mb-2' for="password">Password</label>
                             <input className='py-3 rounded-md bg-[#F2F2F2] px-5' type="password" name="password" id="" placeholder='Enter password' />
                         </div>
 
-                        <div className='flex mt-[35px] sm:w-2/3 w-full mx-auto justify-between items-center'>
+                        <div className='flex mt-[35px] w-full mx-auto justify-between items-center'>
 
-                            <div class="checkbpox_container w-1/3">
+                            <div class="checkbpox_container w-[30%]">
                                 <input type="checkbox" />
                             </div>
-                            <div className="flex remember_forget items-center justify-between w-2/3">
+                            <div className="flex remember_forget items-center justify-between w-[70%]">
                                 <span className='remember text-[14px]'>Remember Me</span>
                                 <span className=' text-[14px] text-[#007AFF] cursor-pointer'>Forget Password</span>
                             </div>
                         </div>
-                        <div className="flex sm:w-2/3 w-full mx-auto flex-col">
+                        <div className="flex w-full mx-auto flex-col">
 
-                            <button className='btn bg-[#007AFF] my-[20px] border-0 text-white font-semibold text-[17px] hover:bg-transparent hover:border-[#007AFF] hover:border hover:text-[#007AFF]' type="submit">Sign In</button>
+                            <button className='btn bg-[#007AFF] my-[20px] border-0 text-white font-semibold text-[17px] hover:bg-transparent hover:border-[#007AFF] hover:border hover:text-[#007AFF] ' type="submit">Sign In</button>
 
                             <div className="divider">or</div>
                             <button className='flex py-[4px] justify-center items-center border border-slate-600 rounded-lg hover:bg-[#333333] hover:text-white transition-all delay-75 ease-in-out'>
@@ -48,7 +48,7 @@ const FigmaLogin = () => {
                                 <span className='font-semibold text-[17px] ml-3 '>Continue with google</span>
                             </button>
                         </div>
-                        <h1 className='sm:w-2/3 w-full text-[15px] mx-auto text-center mt-[20px]'>Don't have an account? <span className='text-[#007AFF] ml-3 cursor-pointer'>Sign Up</span></h1>
+                        <h1 className=' w-full text-[15px] mx-auto text-center mt-[20px]'>Don't have an account? <span className='text-[#007AFF] ml-3 cursor-pointer'>Sign Up</span></h1>
 
                     </form>
 
