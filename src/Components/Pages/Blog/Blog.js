b78e5b528431b36d64202e1266e8974a847a07bb
Md Imran Hossen <freelancerimran81@gmail.com>
2022-08-27 14:12:54 +0600

    trending section done on blog page

--- src/Components/Pages/Blog/Blog.js
+++ src/Components/Pages/Blog/Blog.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import SharedNav from '../Shared/SharedNav';
+import Trending from './Trending/Trending';
 
 const Blog = () => {
     return (
-        <div>
-            this is blog page
-        </div>
+        <>
+            <SharedNav />
+            <div className='mid-container h-[100vh]'>
+                <Trending />
+            </div>
+        </>
     );
 };
 
