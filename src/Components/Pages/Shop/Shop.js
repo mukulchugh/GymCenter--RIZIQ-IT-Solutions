95c696fe2cb1f4e2650c0c9d1b1237f3fad427a4
Md Imran Hossen <freelancerimran81@gmail.com>
2022-08-27 20:58:46 +0600

    shop page done

--- src/Components/Pages/Shop/Shop.js
+++ src/Components/Pages/Shop/Shop.js
@@ -1,12 +1,210 @@
 import React from 'react';
 import SharedNav from '../Shared/SharedNav';
+import img from '../../../assets/Image/HomeShop/glo-p-img.png'
+import { TbCurrencyTaka } from 'react-icons/tb';
 
 const Shop = () => {
     return (
         <>
             <SharedNav />
-            <div>
-                this is shop page
+            <div className='mid-container'>
+                <div className='my-16  grid grid-cols-5 gap-6'>
+                    <div class="card bg-accent border rounded">
+                        <figure class="px-5 pt-5">
+                            <img src={img} alt="Shoes" class="w-full" />
+                        </figure>
+                        <div class="card-body items-center text-center px-0 pb-0 pt-3">
+                            <h2 class="card-title font-semibold">Product Name</h2>
+                            <p className='text-xl font-bold text-primary flex'><TbCurrencyTaka className='text-2xl' />199</p>
+                            <p className='text-secondary text-sm mb-2'>05/10 available</p>
+                            <div class="bg-[#FBB848] p-3 w-full cursor-pointer">
+                                <h1 className='font-bold'>Add to cart</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card bg-accent border rounded">
+                        <figure class="px-5 pt-5">
+                            <img src={img} alt="Shoes" class="w-full" />
+                        </figure>
+                        <div class="card-body items-center text-center px-0 pb-0 pt-3">
+                            <h2 class="card-title font-semibold">Product Name</h2>
+                            <p className='text-xl font-bold text-primary flex'><TbCurrencyTaka className='text-2xl' />199</p>
+                            <p className='text-secondary text-sm mb-2'>05/10 available</p>
+                            <div class="bg-[#FBB848] p-3 w-full cursor-pointer">
+                                <h1 className='font-bold'>Add to cart</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card bg-accent border rounded">
+                        <figure class="px-5 pt-5">
+                            <img src={img} alt="Shoes" class="w-full" />
+                        </figure>
+                        <div class="card-body items-center text-center px-0 pb-0 pt-3">
+                            <h2 class="card-title font-semibold">Product Name</h2>
+                            <p className='text-xl font-bold text-primary flex'><TbCurrencyTaka className='text-2xl' />199</p>
+                            <p className='text-secondary text-sm mb-2'>05/10 available</p>
+                            <div class="bg-[#FBB848] p-3 w-full cursor-pointer">
+                                <h1 className='font-bold'>Add to cart</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card bg-accent border rounded">
+                        <figure class="px-5 pt-5">
+                            <img src={img} alt="Shoes" class="w-full" />
+                        </figure>
+                        <div class="card-body items-center text-center px-0 pb-0 pt-3">
+                            <h2 class="card-title font-semibold">Product Name</h2>
+                            <p className='text-xl font-bold text-primary flex'><TbCurrencyTaka className='text-2xl' />199</p>
+                            <p className='text-secondary text-sm mb-2'>05/10 available</p>
+                            <div class="bg-[#FBB848] p-3 w-full cursor-pointer">
+                                <h1 className='font-bold'>Add to cart</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card bg-accent border rounded">
+                        <figure class="px-5 pt-5">
+                            <img src={img} alt="Shoes" class="w-full" />
+                        </figure>
+                        <div class="card-body items-center text-center px-0 pb-0 pt-3">
+                            <h2 class="card-title font-semibold">Product Name</h2>
+                            <p className='text-xl font-bold text-primary flex'><TbCurrencyTaka className='text-2xl' />199</p>
+                            <p className='text-secondary text-sm mb-2'>05/10 available</p>
+                            <div class="bg-[#FBB848] p-3 w-full cursor-pointer">
+                                <h1 className='font-bold'>Add to cart</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card bg-accent border rounded">
+                        <figure class="px-5 pt-5">
+                            <img src={img} alt="Shoes" class="w-full" />
+                        </figure>
+                        <div class="card-body items-center text-center px-0 pb-0 pt-3">
+                            <h2 class="card-title font-semibold">Product Name</h2>
+                            <p className='text-xl font-bold text-primary flex'><TbCurrencyTaka className='text-2xl' />199</p>
+                            <p className='text-secondary text-sm mb-2'>05/10 available</p>
+                            <div class="bg-[#FBB848] p-3 w-full cursor-pointer">
+                                <h1 className='font-bold'>Add to cart</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card bg-accent border rounded">
+                        <figure class="px-5 pt-5">
+                            <img src={img} alt="Shoes" class="w-full" />
+                        </figure>
+                        <div class="card-body items-center text-center px-0 pb-0 pt-3">
+                            <h2 class="card-title font-semibold">Product Name</h2>
+                            <p className='text-xl font-bold text-primary flex'><TbCurrencyTaka className='text-2xl' />199</p>
+                            <p className='text-secondary text-sm mb-2'>05/10 available</p>
+                            <div class="bg-[#FBB848] p-3 w-full cursor-pointer">
+                                <h1 className='font-bold'>Add to cart</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card bg-accent border rounded">
+                        <figure class="px-5 pt-5">
+                            <img src={img} alt="Shoes" class="w-full" />
+                        </figure>
+                        <div class="card-body items-center text-center px-0 pb-0 pt-3">
+                            <h2 class="card-title font-semibold">Product Name</h2>
+                            <p className='text-xl font-bold text-primary flex'><TbCurrencyTaka className='text-2xl' />199</p>
+                            <p className='text-secondary text-sm mb-2'>05/10 available</p>
+                            <div class="bg-[#FBB848] p-3 w-full cursor-pointer">
+                                <h1 className='font-bold'>Add to cart</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card bg-accent border rounded">
+                        <figure class="px-5 pt-5">
+                            <img src={img} alt="Shoes" class="w-full" />
+                        </figure>
+                        <div class="card-body items-center text-center px-0 pb-0 pt-3">
+                            <h2 class="card-title font-semibold">Product Name</h2>
+                            <p className='text-xl font-bold text-primary flex'><TbCurrencyTaka className='text-2xl' />199</p>
+                            <p className='text-secondary text-sm mb-2'>05/10 available</p>
+                            <div class="bg-[#FBB848] p-3 w-full cursor-pointer">
+                                <h1 className='font-bold'>Add to cart</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card bg-accent border rounded">
+                        <figure class="px-5 pt-5">
+                            <img src={img} alt="Shoes" class="w-full" />
+                        </figure>
+                        <div class="card-body items-center text-center px-0 pb-0 pt-3">
+                            <h2 class="card-title font-semibold">Product Name</h2>
+                            <p className='text-xl font-bold text-primary flex'><TbCurrencyTaka className='text-2xl' />199</p>
+                            <p className='text-secondary text-sm mb-2'>05/10 available</p>
+                            <div class="bg-[#FBB848] p-3 w-full cursor-pointer">
+                                <h1 className='font-bold'>Add to cart</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card bg-accent border rounded">
+                        <figure class="px-5 pt-5">
+                            <img src={img} alt="Shoes" class="w-full" />
+                        </figure>
+                        <div class="card-body items-center text-center px-0 pb-0 pt-3">
+                            <h2 class="card-title font-semibold">Product Name</h2>
+                            <p className='text-xl font-bold text-primary flex'><TbCurrencyTaka className='text-2xl' />199</p>
+                            <p className='text-secondary text-sm mb-2'>05/10 available</p>
+                            <div class="bg-[#FBB848] p-3 w-full cursor-pointer">
+                                <h1 className='font-bold'>Add to cart</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card bg-accent border rounded">
+                        <figure class="px-5 pt-5">
+                            <img src={img} alt="Shoes" class="w-full" />
+                        </figure>
+                        <div class="card-body items-center text-center px-0 pb-0 pt-3">
+                            <h2 class="card-title font-semibold">Product Name</h2>
+                            <p className='text-xl font-bold text-primary flex'><TbCurrencyTaka className='text-2xl' />199</p>
+                            <p className='text-secondary text-sm mb-2'>05/10 available</p>
+                            <div class="bg-[#FBB848] p-3 w-full cursor-pointer">
+                                <h1 className='font-bold'>Add to cart</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card bg-accent border rounded">
+                        <figure class="px-5 pt-5">
+                            <img src={img} alt="Shoes" class="w-full" />
+                        </figure>
+                        <div class="card-body items-center text-center px-0 pb-0 pt-3">
+                            <h2 class="card-title font-semibold">Product Name</h2>
+                            <p className='text-xl font-bold text-primary flex'><TbCurrencyTaka className='text-2xl' />199</p>
+                            <p className='text-secondary text-sm mb-2'>05/10 available</p>
+                            <div class="bg-[#FBB848] p-3 w-full cursor-pointer">
+                                <h1 className='font-bold'>Add to cart</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card bg-accent border rounded">
+                        <figure class="px-5 pt-5">
+                            <img src={img} alt="Shoes" class="w-full" />
+                        </figure>
+                        <div class="card-body items-center text-center px-0 pb-0 pt-3">
+                            <h2 class="card-title font-semibold">Product Name</h2>
+                            <p className='text-xl font-bold text-primary flex'><TbCurrencyTaka className='text-2xl' />199</p>
+                            <p className='text-secondary text-sm mb-2'>05/10 available</p>
+                            <div class="bg-[#FBB848] p-3 w-full cursor-pointer">
+                                <h1 className='font-bold'>Add to cart</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card bg-accent border rounded">
+                        <figure class="px-5 pt-5">
+                            <img src={img} alt="Shoes" class="w-full" />
+                        </figure>
+                        <div class="card-body items-center text-center px-0 pb-0 pt-3">
+                            <h2 class="card-title font-semibold">Product Name</h2>
+                            <p className='text-xl font-bold text-primary flex'><TbCurrencyTaka className='text-2xl' />199</p>
+                            <p className='text-secondary text-sm mb-2'>05/10 available</p>
+                            <div class="bg-[#FBB848] p-3 w-full cursor-pointer">
+                                <h1 className='font-bold'>Add to cart</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
