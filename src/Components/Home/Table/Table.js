60fcfb4b901187ec5a587eb50ef4e87238b153c8
Md Imran Hossen <freelancerimran81@gmail.com>
2022-08-28 09:18:03 +0600

    table issue solved

--- src/Components/Home/Table/Table.js
+++ src/Components/Home/Table/Table.js
@@ -3,16 +3,17 @@ import SingleTable from './SingleTable';
 
 const Table = () => {
     const morningSchedule = [
-        { id: 1, time: '06:00 am', data: 'Cross Fit' },
-        { id: 2, time: '06:30 am', data: 'Cross Fit' },
-        { id: 3, time: '06:00 am', data: 'Cross Fit' },
-        { id: 4, time: '06:30 am', data: 'Cross Fit' },
-        { id: 5, time: '07:00 am', data: 'Cross Fit' },
-        { id: 6, time: '07:30 am', data: 'Cross Fit' },
-        { id: 7, time: '08:00 am', data: 'Cross Fit' },
-        { id: 8, time: '08:30 am', data: 'Cross Fit' },
-        { id: 9, time: '09:00 am', data: 'Cross Fit' },
-        { id: 10, time: '09:30 am', data: 'Cross Fit' }
+        { id: 1, time: '07:00 AM', data: 'Cross Fit' },
+        { id: 2, time: '08:00 AM', data: 'Cross Fit' },
+        { id: 3, time: '09:00 AM', data: 'Cross Fit' },
+        { id: 4, time: '11:30 AM', data: 'Cross Fit' },
+        { id: 5, time: '12:00 PM', data: 'Cross Fit' },
+        { id: 6, time: '03:00 PM', data: 'Cross Fit' },
+        { id: 7, time: '04:00 PM', data: 'Cross Fit' },
+        { id: 8, time: '05:00 PM', data: 'Cross Fit' },
+        { id: 9, time: '06:00 PM', data: 'Cross Fit' },
+        { id: 10, time:'07:00 PM', data: 'Cross Fit' },
+        { id: 11, time:'08:00 PM', data: 'Cross Fit' }
     ];
     return (
         <div className='mid-container mx-auto'>
