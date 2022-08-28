3e2c0215ce5ea5cc440f2c49eec8e2c62cea8c2b
Md Imran Hossen <freelancerimran81@gmail.com>
2022-08-29 02:53:35 +0600

    balance section responsive

--- src/Dashboard/AdminDashboard/Home/AccountsHome.js
+++ src/Dashboard/AdminDashboard/Home/AccountsHome.js
@@ -27,31 +27,31 @@ const AccountsHome = () => {
                 <p className='text-secondary text-sm'>March 2022</p>
             </div>
 
-            <div className='grid grid-cols-3 bg-neutral py-8 mb-8'>
-                <div className='flex items-center justify-center border-r-2 border-white'>
-                    <div className='py-8 '>
-                        <h1 className='font-bold text-xl'>Current Balance</h1>
-                        <h2 className='flex items-center justify-center text-3xl font-semibold mr-4'><TbCurrencyTaka />100</h2>
+            <div className='grid sm:grid-cols-3 bg-neutral sm:py-8 mb-8'>
+                <div className='flex items-center justify-center sm:border-r-2 sm:border-b-0 border-b-2 border-white mb-5'>
+                    <div className='sm:py-8 pt-5 pb-2'>
+                        <h1 className='font-bold text-xl text-center'>Current Balance</h1>
+                        <h2 className='flex items-center justify-center sm:text-3xl text-2xl  font-semibold mr-4'><TbCurrencyTaka />100</h2>
                     </div>
                 </div>
 
-                <div className='flex items-center justify-center border-r-2 border-white relative'>
-                    <div className='bg-primary shadow-xl text-white p-2 rounded cursor-pointer absolute -mt-32 -mr-60'>
+                <div className='flex items-center justify-center sm:border-r-2 sm:border-b-0 border-b-2 border-white relative'>
+                    <div className='bg-primary shadow-xl text-white p-2 rounded cursor-pointer absolute lg:-mt-32 md:-mt-24 sm:-mt-28 -mt-14 lg:-mr-60 md:-mr-52 sm:-mr-40 -mr-60'>
                         <BsPlusLg className=' font-extrabold' />
                     </div>
-                    <div className='text-primary py-8'>
+                    <div className='text-primary sm:py-8  pb-2'>
                         <h1 className='font-bold text-xl'>Total Expense</h1>
-                        <h2 className='flex items-center justify-center text-3xl font-semibold mr-4'><TbCurrencyTaka />100</h2>
+                        <h2 className='flex items-center justify-center sm:text-3xl text-2xl font-semibold mr-4'><TbCurrencyTaka />100</h2>
                     </div>
                 </div>
 
                 <div className='flex items-center justify-center relative'>
-                    <div className='bg-primary shadow-xl text-white p-2 rounded cursor-pointer absolute -mt-32 -mr-60'>
+                    <div className='bg-primary shadow-xl text-white p-2 rounded cursor-pointer absolute lg:-mt-32 md:-mt-24 sm:-mt-28 -mt-10 lg:-mr-60 md:-mr-52 sm:-mr-40 -mr-60'>
                         <BsPlusLg className=' font-extrabold' />
                     </div>
-                    <div className='text-success py-8'>
+                    <div className='text-success sm:py-8 pt-5 pb-4'>
                         <h1 className='font-bold text-xl'>Total Income</h1>
-                        <h2 className='flex items-center justify-center text-3xl font-semibold mr-4'><TbCurrencyTaka />100</h2>
+                        <h2 className='flex items-center justify-center sm:text-3xl text-2xl font-semibold mr-4'><TbCurrencyTaka />100</h2>
                     </div>
                 </div>
             </div>
