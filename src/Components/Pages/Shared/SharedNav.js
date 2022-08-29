ddde054debf99e24f6413d65c78cfd63051483ef
Md Imran Hossen <freelancerimran81@gmail.com>
2022-08-29 16:02:01 +0600

    update

--- src/Components/Pages/Shared/SharedNav.js
+++ src/Components/Pages/Shared/SharedNav.js
@@ -69,7 +69,6 @@ const SharedNav = () => {
                             <div className='lg:hidden '>
                                 {
                                     pathname.includes("dashboard") && (<label htmlFor="my-drawer-2" tabIndex="1" className="">
-                                        {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg> */}
                                         <MdDashboard className='text-xl'/>
                                     </label>)
                                 }
