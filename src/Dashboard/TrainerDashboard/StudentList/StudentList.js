6abfe9bcb7a4cdb056502cc81f1a3be5e36f18e9
Md Imran Hossen <freelancerimran81@gmail.com>
2022-09-06 01:13:44 +0600

    studentsList page

--- src/Dashboard/TrainerDashboard/StudentList/StudentList.js
+++ src/Dashboard/TrainerDashboard/StudentList/StudentList.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import { BiSearch } from 'react-icons/bi';
 import { VscBellDot } from 'react-icons/vsc';
+import { useQuery } from 'react-query';
+import AuthUser from '../../../hooks/AuthUser/AuthUser';
+import Loading from '../../../hooks/Loading/Loading';
 
 const StudentList = () => {
+    const {token} = AuthUser();
+    const today = new Date();
+    const day = today.getDate();
+    const month = today.getMonth();
+    const year = today.getFullYear();
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const monthName = monthNames[month];
+    const date = `${day} ${monthName} ${year}`;
+
+    const { data: students, isLoading, refetch } = useQuery('users', () =>
+        fetch(`https://gym-management97.herokuapp.com/api/all_students_of_trainer`, {
+            method: 'GET',
+            headers: {
+                'authorization': `Bearer ${token}`
+            }
+        }).then(res => res.json())
+    )
+    if (isLoading) {
+        return <Loading />
+    }
+
+    console.log(students.data)
+
     return (
         <div className='p-5 mt-4'>
             <div className='flex justify-between'>
                 <h2 className='text-2xl font-semibold'>Hello, Accounts!</h2>
                 <div className='flex items-center gap-3'>
-                    {/* <p className='text-sm font-bold text-secondary'>{date}</p> */}
+                    <p className='text-sm font-bold text-secondary'>{date}</p>
                     <div className='bg-accent px-3 py-2 rounded cursor-pointer'>
                         <BiSearch className='text-xl ' />
                     </div>
@@ -18,6 +44,11 @@ const StudentList = () => {
 
                 </div>
             </div>
+            <div className='mt-7 border-b-[1px] pb-2 mb-5'>
+                <h1 className='font-bold text-xl'>Student List</h1>
+            </div>
+
+
         </div>
     );
 };
