52f3385ab66ad05bf5c9f4e098e45022bb423260
Md Imran Hossen <freelancerimran81@gmail.com>
2022-09-07 22:51:39 +0600

    menu toggle solve

--- src/Components/Home/Header/Navbar.js
+++ src/Components/Home/Header/Navbar.js
@@ -52,12 +52,7 @@ const Navbar = () => {
             <div className='navbar-end lg:hidden'>
                 <div className="dropdown dropdown-end ">
                     <label onClick={() => setIsOpen(!isOpen)} tabIndex="0" className="btn btn-ghost lg:hidden pr-0">
-                        {
-                            isOpen ? <IoClose className="h-5 w-5" /> :
-                                (
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                                )
-                        }
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
 
                     {
