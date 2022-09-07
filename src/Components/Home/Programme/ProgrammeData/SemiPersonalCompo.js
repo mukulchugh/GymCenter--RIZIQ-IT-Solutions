a49014193229794377398999ec2e3bbce96712ab
Ayon Jodder <ayonjodder177@gmail.com>
2022-09-07 15:40:42 +0600

    Margin added

--- src/Components/Home/Programme/ProgrammeData/SemiPersonalCompo.js
+++ src/Components/Home/Programme/ProgrammeData/SemiPersonalCompo.js
@@ -115,7 +115,7 @@ const SemiPersonalCompo = () => {
             </div>
 
 
-            <div className="schedule mb-10 md:flex items-center">
+            <div className="schedule mt-5 mb-10 md:flex items-center">
                 <div className="schedule_time w-full">
                     <div className="overflow-x-auto">
                         <table className="table table-zebra table_border table-compact w-full">
