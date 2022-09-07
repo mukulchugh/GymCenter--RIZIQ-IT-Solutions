3abb373959dc915e7ce76613fe96eca218397baf
Md Imran Hossen <freelancerimran81@gmail.com>
2022-09-07 11:48:33 +0600

    trainer section update

--- src/Components/Pages/CrossFit/CrossFit.js
+++ src/Components/Pages/CrossFit/CrossFit.js
@@ -44,36 +44,39 @@ const CrossFit = () => {
 
                 <div className='mb-16'>
                     <h1 className='md:text-4xl text-3xl font-bold text-primary mb-6'>Our Experts</h1>
-                    <div className='grid md:grid-cols-2 gap-10'>
-                        <div className='w-full  overflow-hidden rounded-xl'>
+                    <div className='md:flex gap-10 items-center'>
+                        <div className='lg:w-[35%] md:w-[45%] overflow-hidden rounded-xl'>
                             <img className='w-full' src={daif} alt="" />
                         </div>
-                        <div className='flex items-center '>
+                        <div className='lg:w-[65%] md:w-[55%]'>
                             <div>
                                 <h1 className='text-3xl text-primary font-extrabold mb-5 uppercase'>Daif Salemoon </h1>
-                                <p className='font-bold lg:w-[90%] '>Daif is a CrossFit Level-2 trainer. He's coaching CrossFit for the last 5 years. Being a black belt in Karate, he's used to living a disciplined life from an early age. Resultantly, his coaching reflects the values of a balanced lifestyle. As a  personal trainer and nutritionist, he is proficient at navigating clients journey through different stages of change. It's not always about the training or nutrition that is challenging, sometimes people struggle with progress due to setbacks in other areas of their lives. Learning from and overcoming his own challenges, Daif is a life coach who's passionate about  helping people overcome life hurdles, set goals and follow their passion and purpose.</p>
+                                <p className=''>Daif is a CrossFit Level-2 trainer. He's coaching CrossFit for the last 5 years. Being a black belt in Karate, he's used to living a disciplined life from an early age. Resultantly, his coaching reflects the values of a balanced lifestyle. As a  personal trainer and nutritionist, he is proficient at navigating clients journey through different stages of change. It's not always about the training or nutrition that is challenging, sometimes people struggle with progress due to setbacks in other areas of their lives. Learning from and overcoming his own challenges, Daif is a life coach who's passionate about  helping people overcome life hurdles, set goals and follow their passion and purpose.</p>
 
                                 <button className='custom-btn btn-15 mt-5 p-3'>see more</button>
                             </div>
                         </div>
 
                     </div>
-                    <div className='grid md:grid-cols-2 gap-10  mt-14'>
-                    <div className='flex items-center md:order-1 order-2'>
-                        <div>
-                            <h1 className='text-3xl text-primary font-extrabold mb-5'>WHAT IS CROSSFIT?</h1>
-                            <p className='font-bold lg:w-[90%] '>CrossFit is a strength & conditioning program based on general physical preparedness (GPP). In the CrossFit method of training, real life movements like running, lifting, swimming, cycling, carrying loads to distance, climbing, pushing or pulling heavy objects etc. are programmed into effective and fun workouts designed by CrossFit trainers. </p>
-
-                            <button className='custom-btn btn-15 mt-5 p-3'>see more</button>
+                    <div className='md:flex items-center gap-10  mt-14'>
+                        <div className='lg:w-[35%] md:w-[45%] overflow-hidden rounded-xl md:order-2 order-1'>
+                            <img className='w-full' src={shahid} alt="" />
                         </div>
+                        <div className='lg:w-[65%] md:w-[55%] md:order-1 order-2'>
+                            <div>
+                                <h1 className='text-3xl text-primary font-extrabold mb-5 uppercase'>Shahidur Rahman </h1>
+                                <p className=''>Shahid is a CrossFit level-1 trainer. He's passionate about helping people be in control of their lives and he wants to do that by making them move better in their day to day lives. He's working as a coach for the last nine years. He's a certified personal trainer who has a knack for corrective and adaptive exercise and many of our clients with injuries and other physical limitations are reaping the benefit of that. <br /><br />
+                                    These days, busy life leaves little room for exercise for many of us. A short intensive class can solve that. Shahid here is a HIIT certified coach who delivers short intense and fun classes at CFA regularly. <br /><br />
+                                    As it's not always about training and factors like nutrition, sleep, stress and support system plays important roles in a person's growth, Shahid is efficient at not just showing the path but also leading the way every step of the journey.</p>
+
+                                <button className='custom-btn btn-15 mt-5 p-3'>see more</button>
+                            </div>
+                        </div>
+
                     </div>
-                    <div className='w-full overflow-hidden rounded-xl md:order-2 order-1'>
-                        <img className='w-full' src={shahid} alt="" />
-                    </div>
-                </div>
                 </div>
 
-                
+
 
                 <h1 className='md:text-4xl text-3xl font-bold text-primary mb-6'>Recommended <br /> Packages for you</h1>
 
@@ -92,4 +95,7 @@ export default CrossFit;
 // Daif is a CrossFit Level-2 trainer. He's coaching CrossFit for the last 5 years. Being a black belt in Karate, he's used to living a disciplined life from an early age. Resultantly, his coaching reflects the values of a balanced lifestyle. As a  personal trainer and nutritionist, he is proficient at navigating clients journey through different stages of change. It's not always about the training or nutrition that is challenging, sometimes people struggle with progress due to setbacks in other areas of their lives. Learning from and overcoming his own challenges, Daif is a life coach who's passionate about  helping people overcome life hurdles, set goals and follow their passion and purpose. 
 
 
-/* Daif is a CrossFit Level-2 trainer. He's coaching CrossFit for the last 5 years. Being a black belt in Karate, he's used to living a disciplined life from an early age. Resultantly, his coaching reflects the values of a balanced lifestyle. As a  personal trainer and nutritionist, he is proficient at navigating clients journey through different stages of change. It's not always about the training or nutrition that is challenging, sometimes people struggle with progress due to setbacks in other areas of their lives. Learning from and overcoming his own challenges, Daif is a life coach who's passionate about  helping people overcome life hurdles, set goals and follow their passion and purpose.  */
+/* Shahid is a CrossFit level-1 trainer. He's passionate about helping people be in control of their lives and he wants to do that by making them move better in their day to day lives. He's working as a coach for the last nine years. He's a certified personal trainer who has a knack for corrective and adaptive exercise and many of our clients with injuries and other physical limitations are reaping the benefit of that. 
+These days, busy life leaves little room for exercise for many of us. A short intensive class can solve that. Shahid here is a HIIT certified coach who delivers short intense and fun classes at CFA regularly. 
+As it's not always about training and factors like nutrition, sleep, stress and support system plays important roles in a person's growth, Shahid is efficient at not just showing the path but also leading the way every step of the journey.
+  */
