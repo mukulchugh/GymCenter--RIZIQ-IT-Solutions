c1243642806f2f30e4a15c57c0810cf0eeb8109a
Md Imran Hossen <freelancerimran81@gmail.com>
2022-09-08 18:31:55 +0600

    crossfit updated

--- src/Components/Pages/CrossFit/CrossFit.js
+++ src/Components/Pages/CrossFit/CrossFit.js
@@ -50,7 +50,7 @@ const CrossFit = () => {
                         </div>
                         <div className='lg:w-[65%] md:w-[55%]'>
                             <div>
-                                <h1 className='text-3xl text-primary font-extrabold mb-5 uppercase'>Daif Salemoon </h1>
+                                <h1 className='text-3xl text-primary font-extrabold mb-5 uppercase md:mt-0 mt-5'>Daif Salemoon </h1>
                                 <p className='text-justify'>Daif is a CrossFit Level-2 trainer. He's coaching CrossFit for the last 5 years. Being a black belt in Karate, he's used to living a disciplined life from an early age. Resultantly, his coaching reflects the values of a balanced lifestyle. As a  personal trainer and nutritionist, he is proficient at navigating clients journey through different stages of change. It's not always about the training or nutrition that is challenging, sometimes people struggle with progress due to setbacks in other areas of their lives. Learning from and overcoming his own challenges, Daif is a life coach who's passionate about  helping people overcome life hurdles, set goals and follow their passion and purpose.</p>
 
                                 <button className='custom-btn btn-15 mt-5 p-3'>see more</button>
@@ -64,7 +64,7 @@ const CrossFit = () => {
                         </div>
                         <div className='lg:w-[65%] md:w-[55%] md:order-1 order-2'>
                             <div>
-                                <h1 className='text-3xl text-primary font-extrabold mb-5 uppercase'>Shahidur Rahman </h1>
+                                <h1 className='text-3xl text-primary font-extrabold mb-5 uppercase md:mt-0 mt-5'>Shahidur Rahman </h1>
                                 <p className='text-justify'>Shahid is a CrossFit level-1 trainer. He's passionate about helping people be in control of their lives and he wants to do that by making them move better in their day to day lives. He's working as a coach for the last nine years. He's a certified personal trainer who has a knack for corrective and adaptive exercise and many of our clients with injuries and other physical limitations are reaping the benefit of that. <br /><br />
                                     These days, busy life leaves little room for exercise for many of us. A short intensive class can solve that. Shahid here is a HIIT certified coach who delivers short intense and fun classes at CFA regularly. <br /><br />
                                     As it's not always about training and factors like nutrition, sleep, stress and support system plays important roles in a person's growth, Shahid is efficient at not just showing the path but also leading the way every step of the journey.</p>
