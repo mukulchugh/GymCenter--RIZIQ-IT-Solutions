61aec920d6c1efb6be9e9037b0a013550870a611
Md Imran Hossen <freelancerimran81@gmail.com>
2022-09-09 19:34:36 +0600

    dfa

--- src/Components/Pages/Shop/Shop.js
+++ src/Components/Pages/Shop/Shop.js
@@ -15,15 +15,21 @@ const Shop = () => {
     const [pageNumber, setPageNumber] = useState(1);
     const [allProductsCount, setAllProductsCount] = useState(0);
     const [products, setProducts] = useState([]);
-   
+    const [searchResult, setSearchResult] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
 
-    
+    const handleSearch = (e) => {
+
+        const searchResult = products?.filter(product => product?.name.toLowerCase().includes(searchValue.toLowerCase()));
+        setSearchResult(searchResult);
+        console.log(searchResult)
+    }
+
+
     useEffect(() => {
         fetch(`https://gym-management97.herokuapp.com/api/products`)
             .then(res => res.json())
             .then(data => {
-                console.log(data)
-                // console.log(data.data.length)
                 const count = Math.ceil(data.data.length / productsCount)
                 setAllProductsCount(data.data.length)
                 setPageCount(count)
@@ -57,6 +63,18 @@ const Shop = () => {
         <>
             <SharedNav />
             <div className='mid-container h-[100vh]'>
+                <div className="form-control">
+                    <div className="input-group">
+                        <input onChange={(e) => setSearchValue(e.target.value)} type="text" placeholder="Search…" className="input input-bordered" />
+                        <button
+                            onClick={handleSearch}
+                            className="btn btn-square">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                        </button>
+                    </div>
+                </div>
+
+
                 <div className='my-16 grid lg:grid-cols-4 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 sm:gap-6 gap-4'>
                     {
                         products?.data?.map(product => <Products
