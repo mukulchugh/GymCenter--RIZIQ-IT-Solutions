e5bff29b5cf1e4af744e3051e24c3542eb0e0b7c
Md Imran Hossen <freelancerimran81@gmail.com>
2022-09-13 15:55:41 +0600

    payment method

--- src/Components/Pages/PaymentCard/PaymentCard.js
+++ src/Components/Pages/PaymentCard/PaymentCard.js
@@ -7,9 +7,12 @@ import { Link } from 'react-router-dom';
 import AuthUser from '../../../hooks/AuthUser/AuthUser';
 import { useQuery } from 'react-query';
 import Loading from '../../../hooks/Loading/Loading';
+import toast from 'react-hot-toast';
+import { useForm } from 'react-hook-form';
 
 const PaymentCard = () => {
-    const {token} = AuthUser()
+    const { token } = AuthUser();
+    const { register, formState: { errors }, handleSubmit, trigger, reset } = useForm();
 
     const { data: cartProducts, isLoading, refetch } = useQuery('users', () =>
         fetch(`https://gym-management97.herokuapp.com/api/product_cart/`, {
@@ -25,9 +28,46 @@ const PaymentCard = () => {
 
 
     const initialValue = 0;
-    const totalPrice = cartProducts?.data?.reduce((accumulator,current) => accumulator + current.sub_total_price * current.quantity, initialValue)
+    const totalPrice = cartProducts?.data?.reduce((accumulator, current) => accumulator + current.sub_total_price * current.quantity, initialValue)
     // get total quantity 
-    const totalQuantity = cartProducts?.data?.reduce((accumulator,current) => accumulator + current.quantity, initialValue)
+    const totalQuantity = cartProducts?.data?.reduce((accumulator, current) => accumulator + current.quantity, initialValue)
+
+
+    // confirm order
+    const handleConfirmOrder = (data) => {
+        console.log(data)
+        fetch('https://gym-management97.herokuapp.com/api/product_orders/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                address: data.address,
+                city: data.city,
+                email: data.email,
+                phone: data.phone,
+                sub_total_price: 1400,
+                delivery_charge: 40,
+                payment_type: 'cash_on_delivery',
+            })
+        })
+            .then((res) => res.json())
+            .then(data => {
+                if (!data.success) {
+                    if (data.error === 'Something went wrong') {
+                        toast.error('User with this email already exists.')
+                        return;
+                    } else if (data.error) {
+                        toast.error(data.error)
+                        return;
+                    }
+                } else {
+                    toast.success('Product Order Successfully')
+                }
+            })
+            .catch(err => console.log(err))
+    }
 
 
     return (
@@ -56,37 +96,99 @@ const PaymentCard = () => {
                         </div>
                         <div>
                             <h2 className='font-semibold mt-5 text-xl'>Billing Info</h2>
-                            <form className=' mt-5'>
+                            <form
+                                onSubmit={handleSubmit(handleConfirmOrder)}
+                                className=' mt-5'>
                                 <div className='grid grid-cols-2 gap-5'>
-                                    <div className='w-full'>
-                                        <label className='text-gray-500' htmlFor="">Full Name</label>
-                                        <input className='w-full border rounded-md p-2' type="text" />
+                                    <div className="flex  w-full mx-auto flex-col">
+                                        <label className='text-[#747474] text-sm font-medium ml-1 mb-2'>Name</label>
+                                        <input className='py-3 px-5 bg-[#F2F2F2] rounded-md focus:outline-0' type="name" name="name" id="" placeholder='Your Name'
+                                            {...register("name", {
+                                                required: 'Name is required',
+                                                pattern: {
+                                                    value: 3,
+                                                    message: 'Name must be at least 3 characters'
+                                                }
+                                            })}
+                                            onKeyUp={(e) => {
+                                                trigger('name')
+                                            }}
+                                        />
+                                        <small className='text-[#FF4B2B] text-xs ml-2 font-medium my-2'>{errors?.name?.message}</small>
+                                    </div>
+                                    <div className="flex  w-full mx-auto flex-col">
+                                        <label className='text-[#747474] text-sm font-medium ml-1 mb-2'>Address</label>
+                                        <input className='py-3 px-5 bg-[#F2F2F2] rounded-md focus:outline-0' type="text" name="address" id="" placeholder='Your Address'
+                                            {...register("address", {
+                                                required: 'Address is required',
+                                                pattern: {
+                                                    value: 3,
+                                                    message: 'Address must be at least 3 characters'
+                                                }
+                                            })}
+                                            onKeyUp={(e) => {
+                                                trigger('address')
+                                            }}
+                                        />
+                                        <small className='text-[#FF4B2B] text-xs ml-2 font-medium my-2'>{errors?.address?.message}</small>
                                     </div>
 
-                                    <div>
-                                        <label className='text-gray-500 mt-5' htmlFor="">Billing Address</label>
-                                        <input className='w-full border rounded-md p-2' type="text" />
+                                    <div className="flex  w-full mx-auto flex-col">
+                                        <label className='text-[#747474] text-sm font-medium ml-1 mb-2'>Email</label>
+                                        <input className='py-3 px-5 bg-[#F2F2F2] rounded-md focus:outline-0' type="email" name="email" id="" placeholder='Your Email'
+                                            {...register("email", {
+                                                required: 'Email is required',
+                                                pattern: {
+                                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                                    message: "Please enter a valid Email"
+                                                }
+                                            })}
+                                            onKeyUp={(e) => {
+                                                trigger('email')
+                                            }}
+                                        />
+                                        <small className='text-[#FF4B2B] text-xs ml-2 font-medium my-2'>{errors?.email?.message}</small>
                                     </div>
 
-                                    <div>
-                                        <label className='text-gray-500 mt-5' htmlFor="">Email</label>
-                                        <input className='w-full border rounded-md p-2' type="email" />
+                                    <div className="flex  w-full mx-auto flex-col">
+                                        <label className='text-[#747474] text-sm font-medium ml-1 mb-2'>City</label>
+                                        <input className='py-3 px-5 bg-[#F2F2F2] rounded-md focus:outline-0' type="text" name="city" id="" placeholder='City'
+                                            {...register("city", {
+                                                required: 'City is required',
+                                                pattern: {
+                                                    value: 3,
+                                                    message: 'City Name must be at least 3 characters'
+                                                }
+                                            })}
+                                            onKeyUp={(e) => {
+                                                trigger('city')
+                                            }}
+                                        />
+                                        <small className='text-[#FF4B2B] text-xs ml-2 font-medium my-2'>{errors?.city?.message}</small>
                                     </div>
 
-                                    <div>
-                                        <label className='text-gray-500 mt-5' htmlFor="">City</label>
-                                        <input className='w-full border rounded-md p-2' type="text" />
-                                    </div>
-
-                                    <div>
-                                        <label className='text-gray-500 mt-5' htmlFor="">Phone</label>
-                                        <input className='w-full border rounded-md p-2' type="phone" />
+                                    <div className="flex  w-full mx-auto flex-col">
+                                        <label className='text-[#747474] text-sm font-medium ml-1 mb-2' >Phone</label>
+                                        <input className='py-3 rounded-md bg-[#F2F2F2] px-5 focus:outline-0' type="text" name="phone" id="" placeholder='Enter Phone Number'
+                                            {...register('phone', {
+                                                required: 'Phone is required',
+                                                minLength: {
+                                                    value: 11,
+                                                    message: 'Minimum 11 character required'
+                                                }
+                                            })}
+                                            onKeyUp={() => {
+                                                trigger('phone')
+                                            }}
+                                        />
+                                        <small className='text-[#FF4B2B] ml-2 text-xs font-medium my-2'>{errors?.phone?.message}</small>
                                     </div>
 
                                 </div>
                                 <div className='mt-10 flex items-center gap-5'>
                                     <Link className='bg-accent py-3 px-8 border rounded' to='/shop'>Back to Shop</Link>
-                                    <button className='btn btn-primary ' type="submit">Confirm Order</button>
+                                    <button
+                                        className='btn btn-primary ' type="submit">Confirm Order</button>
                                 </div>
                             </form>
                         </div>
